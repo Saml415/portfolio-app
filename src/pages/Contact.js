6147c2fc8bff400f75ed10d81f50192f3ab05db7
Sam Lloyd <bobdoe784@gmail.com>
2022-01-06 16:48:22 -0800

    Clean code and reset for deployment

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Form, Button, Row, Col, Image } from "react-bootstrap";
+import { Form, Button } from "react-bootstrap";
 import Footer from "../components/Footer";
 import "./Contact.css";
 import { validateEmail } from "../utils/helper";
